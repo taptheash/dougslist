242a810844bd4efbf9949bf0021b05bec4c8078b
taptheash <taptheash@gmail.com>
2026-04-30 16:42:12 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,18 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAI7q0TX9kC9YSGxbmsRyfoYu2QOwof4lo",
-  authDomain: "dougs-cookbook.firebaseapp.com",
-  projectId: "dougs-cookbook",
-  storageBucket: "dougs-cookbook.firebasestorage.app",
-  messagingSenderId: "424577298187",
-  appId: "1:424577298187:web:cd484271ba8c585c903c0e",
-  measurementId: "G-RR94G9DBGT"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
